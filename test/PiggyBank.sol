a7dd66c67bb309c6c2813d2d9068213afdb03f6b
Rumena Staneva <rumenastaneva2106@gmail.com>
2025-12-01 10:57:49 +0200

    added additional tests

--- test/PiggyBank.sol
+++ test/PiggyBank.sol
@@ -269,4 +269,199 @@ contract PiggyBankTest is Test {
         assertEq(finalRewards, rewards);
         vm.stopPrank();
     }
+
+    function testCanClaimRewards() public {
+        address user = makeAddr("user");
+        vm.deal(user, 1 ether);
+        vm.startPrank(user);
+        piggyBank.deposit{value: 0.1 ether}();
+
+        // Fast forward time by 1 month to accrue rewards
+        vm.warp(block.timestamp + 30 days);
+        bool canClaim = piggyBank.canClaimRewards(user);
+        assertTrue(piggyBank.canClaimRewards(user));
+        vm.stopPrank();
+    }
+
+    function testWithdrawWhenAmountExceedsDepositButCanObtainFromRewards()
+        public
+    {
+        address user = makeAddr("user");
+        vm.deal(user, 1 ether);
+        vm.startPrank(user);
+        piggyBank.deposit{value: 0.1 ether}();
+
+        // Fast forward time by 1 month to accrue rewards
+        vm.warp(block.timestamp + 30 days);
+
+        // Get user's current deposit and rewards from the contract
+        (uint256 userDeposit, uint256 rewards) = piggyBank.getUserBalance(user);
+
+        uint256 totalAvailable = userDeposit + rewards;
+
+        // Attempt to withdraw an amount greater than deposit but less than total available
+        uint256 withdrawAmount = userDeposit + (rewards / 2);
+        piggyBank.withdraw(withdrawAmount);
+
+        // After withdrawal, check that deposit is zero and rewards reduced accordingly
+        (uint256 finalDeposit, uint256 finalRewards) = piggyBank.getUserBalance(
+            user
+        );
+        assertEq(finalDeposit, 0);
+        assertEq(finalRewards, rewards - (withdrawAmount - userDeposit));
+        vm.stopPrank();
+    }
+
+    function testWithdrawUsingRewardsBeforeLockReverts() public {
+        address user = makeAddr("user");
+        vm.deal(user, 1 ether);
+
+        vm.startPrank(user);
+        piggyBank.deposit{value: 0.1 ether}();
+
+        vm.warp(block.timestamp + 10 days);
+        (uint256 userDeposit, uint256 rewards) = piggyBank.getUserBalance(user);
+        uint256 amount = userDeposit + rewards / 2;
+
+        vm.expectRevert();
+        piggyBank.withdraw(amount);
+        vm.stopPrank();
+    }
+
+    function testWithdrawUsingRewardsWithInsufficientRewardPoolReverts()
+        public
+    {
+        address user = makeAddr("user");
+        vm.deal(user, 10 ether);
+        vm.startPrank(OWNER);
+        // As owner, boost reward rate a lot
+        piggyBank.setRewardRate(10_000); // 100% APR
+        vm.stopPrank();
+
+        // Now user deposits; this also seeds a tiny rewardPool from fees
+        vm.startPrank(user);
+        piggyBank.deposit{value: 1 ether}();
+        vm.stopPrank();
+
+        // Warp long enough so theoretical rewards >> rewardPool
+        vm.warp(block.timestamp + 365 days * 10); // 10 years
+
+        (uint256 userDeposit, uint256 rewards) = piggyBank.getUserBalance(user);
+
+        // Sanity: rewards should be > rewardPool so we can create fromRewards > rewardPool
+        uint256 pool = piggyBank.rewardPool();
+
+        // We want:
+        // - amount > deposit (use rewards)
+        // - amount <= deposit + rewards (avoid CannotWithdrawMoreThanAvailable)
+        // - fromRewards = amount - deposit > rewardPool
+        uint256 fromRewards = pool + 1;
+        uint256 amount = userDeposit + fromRewards;
+
+        // Just to be safe:
+        assertLe(
+            amount,
+            userDeposit + rewards,
+            "amount must be <= totalAvailable"
+        );
+
+        vm.startPrank(user);
+        vm.expectRevert();
+        piggyBank.withdraw(amount);
+        vm.stopPrank();
+    }
+
+    function testClaimRewardsWithoutDepositReverts() public {
+        address user = makeAddr("user");
+        vm.deal(user, 1 ether);
+        vm.startPrank(user);
+        vm.expectRevert();
+        piggyBank.claimRewards();
+        vm.stopPrank();
+    }
+
+    function testClaimRewardsBeforeMinLockPeriodReverts() public {
+        address user = makeAddr("user");
+        vm.deal(user, 1 ether);
+        vm.startPrank(user);
+        piggyBank.deposit{value: 0.1 ether}();
+
+        // Fast forward time by less than min lock period
+        vm.warp(block.timestamp + 15 days);
+
+        vm.expectRevert();
+        piggyBank.claimRewards();
+        vm.stopPrank();
+    }
+
+    function testClaimRewardsSuccessfully() public {
+        address user = makeAddr("user");
+        vm.deal(user, 1 ether);
+        vm.startPrank(user);
+        piggyBank.deposit{value: 0.1 ether}();
+
+        // Fast forward time by more than min lock period
+        vm.warp(block.timestamp + 31 days);
+
+        // Get rewards before claiming
+        (, uint256 rewardsBefore) = piggyBank.getUserBalance(user);
+
+        // Claim rewards
+        piggyBank.claimRewards();
+
+        // Get rewards after claiming
+        (, uint256 rewardsAfter) = piggyBank.getUserBalance(user);
+
+        // After claiming, rewards should be zero
+        assertEq(rewardsAfter, 0);
+        assertGt(rewardsBefore, 0);
+        vm.stopPrank();
+    }
+
+    function testSetRewardRateByNonOwnerReverts() public {
+        address nonOwner = makeAddr("nonOwner");
+        vm.prank(nonOwner);
+        vm.expectRevert();
+        piggyBank.setRewardRate(500);
+    }
+
+    function testSetRewardRateByOwner() public {
+        uint256 newRate = 500;
+        vm.prank(OWNER);
+        piggyBank.setRewardRate(newRate);
+        uint256 currentRate = piggyBank.rewardRate();
+        assertEq(currentRate, newRate);
+    }
+
+    function testGetMinLockPeriod() public {
+        uint256 minLockPeriod = piggyBank.getMinLockPeriod();
+        assertEq(minLockPeriod, 30 days);
+    }
+
+    function testGetMinDepositAmount() public {
+        uint256 minDeposit = piggyBank.getMinDepositAmount();
+        assertEq(minDeposit, 0.01 ether);
+    }
+
+    function testCalculateRewardsViewFunction() public {
+        address user = makeAddr("user");
+        vm.deal(user, 1 ether);
+        vm.startPrank(user);
+        piggyBank.deposit{value: 0.1 ether}();
+
+        vm.warp(block.timestamp + 30 days);
+
+        uint256 rewards = piggyBank.calculateRewards(user);
+
+        (uint256 userDeposit, ) = piggyBank.getUserBalance(user);
+
+        uint256 rewardRate = piggyBank.rewardRate();
+
+        uint256 timeElapsed = 30 days;
+        uint256 expectedRewards = (userDeposit * rewardRate * timeElapsed) /
+            (10000 * SECONDS_PER_YEAR);
+
+        assertApproxEqAbs(rewards, expectedRewards, 1); // 1 wei tolerance
+        vm.stopPrank();
+    }
 }
